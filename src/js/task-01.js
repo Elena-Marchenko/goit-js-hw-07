778b4718a366d5c67dc5bf1bca8cf74a3e03cb0c
Elena-Marchenko <Leawest91@gmail.com>
2021-04-06 21:21:27 +0300

    Revert "Делает 3 задание"
    This reverts commit ca7145ee6b18b0366ebb33131c4b1042a9941c71.

--- src/js/task-01.js
+++
@@ -1,6 +0,0 @@
-// const navEl  = document.querySelectorAll("li.item");
-// console.log(`Всписке ${navEl.length} категории.`);
-// navEl.forEach(element => {
-//     console.log(`Категория: ${element.querySelector("h2").textContent} 
-//     Количество элементов: ${element.querySelectorAll("li").length}`);
-// });
