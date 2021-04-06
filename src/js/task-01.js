f6cb1be3ce4d47486fb2e0ab040bbf0cb6c8b050
Elena-Marchenko <Leawest91@gmail.com>
2021-04-06 22:02:01 +0300

    Делает правки 3го задания

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,6 +1,6 @@
-// const navEl  = document.querySelectorAll("li.item");
-// console.log(`Всписке ${navEl.length} категории.`);
-// navEl.forEach(element => {
-//     console.log(`Категория: ${element.querySelector("h2").textContent} 
-//     Количество элементов: ${element.querySelectorAll("li").length}`);
-// });
+const navEl  = document.querySelectorAll("li.item");
+console.log(`Всписке ${navEl.length} категории.`);
+navEl.forEach(element => {
+    console.log(`Категория: ${element.querySelector("h2").textContent} 
+    Количество элементов: ${element.querySelectorAll("li").length}`);
+});
